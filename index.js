6fada68402db142e7f393514b87f4d149cd78d78
ls1955 <xxlsxx1955@gmail.com>
2023-07-07 19:55:09 +0800

    Style .top-navigation-container
    Include styles during light or dark mode.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const imgs = document.querySelectorAll("img")
 const svgs = document.querySelectorAll("svg")
 const themeBtn = document.querySelector("#theme-switcher")
+const topNavContainer = document.querySelector(".top-navigation-container")
 
 themeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode")
+    topNavContainer.classList.toggle("dark-mode")
 
     // NOTE: Be sure to only apply class on intended components
     imgs.forEach(img => img.classList.toggle("dark-mode"))
@@ -12,6 +14,7 @@ themeBtn.addEventListener("click", () => {
 
 if (window.matchMedia("(prefers-color-scheme: dark)")) {
     document.body.classList.toggle("dark-mode")
+    topNavContainer.classList.toggle("dark-mode")
 
     imgs.forEach(img => img.classList.toggle("dark-mode"))
     svgs.forEach(svg => svg.classList.toggle("dark-mode"))
